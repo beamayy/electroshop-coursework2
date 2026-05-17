280638de407ddc96ee20e771ef99870fd9651746
beamay <beamayoff@gmail.com>
2026-05-17 13:09:51 +0300

    upd 1.3

--- src/main/java/by/bsuir/electroshop/server/service/InventoryService.java
+++ src/main/java/by/bsuir/electroshop/server/service/InventoryService.java
@@ -42,7 +42,7 @@ public class InventoryService {
     public Response deleteProduct(int id) {
         try {
             productRepository.deleteById(id);
-            return Response.ok("Товар удалён");
+            return Response.ok("Товар удалён",null);
         } catch (Exception e) {
             return Response.error("Ошибка удаления: " + e.getMessage());
         }
